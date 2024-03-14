a6601c1e210af74393c52d90372bb3725d861ee1
Carson Gross <carsongro@gmail.com>
2024-03-14 16:26:26 -0600

    Initial Commit

---
+++ ImpossibleDream/ImpossibleDreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpossibleDreamApp.swift
+//  ImpossibleDream
+//
+//  Created by Carson Gross on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ImpossibleDreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
